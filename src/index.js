ee0f1d4e91ec1834d71455d5e07dcbc1e7c89477
Ivan Gawr <ivangawr1993>
2020-10-06 23:24:47 +0300

    feat: decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrs = bracketsConfig.map(e => e.join(''))
+  for (let i = 0; i < arrs.length; i++) {
+    if(str.includes(arrs[i])) {
+      str = str.replace(arrs[i], '');
+      i=-1;
+    }
+  }
+  return str ? false : true;
 }
